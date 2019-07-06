4268b261b6d43fc2a443a01345deb0ca09a34ec7
alamnc30 <pintunc30@gmail.com>
2019-07-06 00:08:06 -0400

    Adding entire project alone with loop and while loop file

---
+++ src/learnJava/Loop.java
@@ -0,0 +1,23 @@
+package learnJava;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("First Loop Started");
+		
+		for (int i= 1; i<=20; i=i +1){
+			
+			System.out.println("The value of i " +i);
+		}
+		System.out.println("Loop End");		
+	
+	}}
+
+
+	
+	
+
+	
+	
---
+++ src/learnJava/whileloop.java
@@ -0,0 +1,23 @@
+package learnJava;
+
+public class whileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int count = 30;
+		
+		while (count<=80){
+			
+			System.out.println("Ticket value is " + count);
+			
+			//count = count+ 3;
+			count++;
+		}
+		
+		
+		
+	}
+
+}
